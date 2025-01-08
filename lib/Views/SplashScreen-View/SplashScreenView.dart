ef4689e88c1fbee6ead518862429f12833fc1587
Momina Tufail <momnatufail30@gmail.com>
2025-01-08 18:49:57 +0500

    delayed time updated

--- lib/Views/SplashScreen-View/SplashScreenView.dart
+++ lib/Views/SplashScreen-View/SplashScreenView.dart
@@ -13,7 +13,7 @@ class _SplashscreenState extends State<Splashscreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.delayed(Duration(seconds: 6), () {
+    Future.delayed(Duration(seconds: 5), () {
       Navigator.push(
           context, MaterialPageRoute(builder: (context) => HomeView()));
     });
