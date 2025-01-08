4e730e09a0a0e16d658967b99883c3db87a2a7b6
Momina Tufail <momnatufail30@gmail.com>
2025-01-08 16:00:59 +0500

    asset added

--- lib/Views/SplashScreen-View/SplashScreenView.dart
+++ lib/Views/SplashScreen-View/SplashScreenView.dart
@@ -21,13 +21,14 @@ class _SplashscreenState extends State<Splashscreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,
+    return Scaffold(backgroundColor: Colors.purple.shade50,
       body: Padding(
         padding: EdgeInsets.only(left: 20, top: 150, right: 20, bottom: 100,),
         child: Center(
           child: Column(children: [
-            // Icon(Icons.facebook_rounded,color: Colors.blue,),
-            Text('Momina Tufail Web Portfolio',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+            SizedBox(height: 80,),
+            Center(child: Text('Momina Tufail Web Portfolio',style: TextStyle(
+                color: Colors.purple,fontWeight: FontWeight.w900),)),
             // SizedBox(height: 50,),
             // Text('Submitted by Momina Tufail'),
           ]
