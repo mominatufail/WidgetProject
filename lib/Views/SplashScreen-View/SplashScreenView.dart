4c81dddcfe177e55f36be5eee9a2fc6507fcc9bc
Momina Tufail <momnatufail30@gmail.com>
2025-01-09 18:45:22 +0500

    code updated

--- lib/Views/SplashScreen-View/SplashScreenView.dart
+++ lib/Views/SplashScreen-View/SplashScreenView.dart
@@ -23,7 +23,7 @@ class _SplashscreenState extends State<Splashscreen> {
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.purple.shade50,
       body: Padding(
-        padding: EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 100,),
+        padding: EdgeInsets.only(left: 20, top: 150, right: 20, bottom: 100,),
         child: Center(
           child: Center(
             child: Column(children: [
